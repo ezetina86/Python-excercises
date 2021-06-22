45f3629ab0d786705d7768e4d2c13722be7ae6b0
Henry <jenzetin@gmail.com>
2021-06-22 08:23:35 -0500

    Working with variables

--- Class1/main.py
+++ Class1/main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+# print("Hello World")
+
+# setting variables
+x = 10
+y = 2
+z = x + y
+
+print(x)
+print(y)
+print(z)
+
+# The  id  function helps to know the memory address
+id(x)
+print(id(x))
+print(id(y))
+print(id(z))
