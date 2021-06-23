0df2b757dea753dc36af1a057de90ec92c7b464b
Henry <jenzetin@gmail.com>
2021-06-23 05:57:03 -0500

    Working with inputs and casts

--- Class1/main.py
+++ Class1/main.py
@@ -1,9 +1,13 @@
-# Bool Types
+# Input function
+result = input("Please write a message:")
+print(result)
+print("The message is:", result)
 
-var = True
-var1 = False
-var2 = 3 > 2
 
-print(var)
-print(var1)
-print(var2)
+num1 = int(input("First number:"))
+num2 = int(input("Second number:"))
+result = num1 + num2
+print("The result is:",result)
+
+
+print("End of the code")
