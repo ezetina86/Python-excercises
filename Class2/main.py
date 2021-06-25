d0c157678d4aff0ea91598952dbae8ff8d548e4e
Henry <jenzetin@gmail.com>
2021-06-25 07:21:39 -0500

    commit

---
+++ Class2/main.py
@@ -0,0 +1,18 @@
+opA = 3
+opB = 2
+
+add = opA + opB
+dif = opA - opB
+mult = opA * opB
+div = opA / opB
+divInt = opA // opB
+res = opA % opB
+exp =  opA ** opB
+
+print('The sum is:', add)
+print(f'The dif is: {dif}')
+print(f'The mult is: {mult}')
+print(f'The div is: {div}')
+print(f'The divInt is: {divInt}')
+print(f'The res is: {res}')
+print(f'The exp is: {exp}')
