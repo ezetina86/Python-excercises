05e3c813165e92ddee632e7f83b53785aca1c9bd
Henry <jenzetin@gmail.com>
2021-06-25 07:19:36 -0500

    Arithmetic operators in Python

--- Class1/main.py
+++ Class1/main.py
@@ -1,13 +1,2 @@
-# Input function
-result = input("Please write a message:")
-print(result)
-print("The message is:", result)
+#Operadores en Pyhon
 
-
-num1 = int(input("First number:"))
-num2 = int(input("Second number:"))
-result = num1 + num2
-print("The result is:",result)
-
-
-print("End of the code")
