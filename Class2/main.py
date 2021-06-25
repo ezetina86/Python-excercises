a3f8dc59d8d9ad25b6f6a6acb466390239da30fd
Henry <jenzetin@gmail.com>
2021-06-25 07:40:20 -0500

    Comparison operators in Python

--- Class2/main.py
+++ Class2/main.py
@@ -1,18 +1,6 @@
-opA = 3
-opB = 2
+# Operadodes de asignacion
+miVAriable = 10
+print(miVAriable)
 
-add = opA + opB
-dif = opA - opB
-mult = opA * opB
-div = opA / opB
-divInt = opA // opB
-res = opA % opB
-exp =  opA ** opB
-
-print('The sum is:', add)
-print(f'The dif is: {dif}')
-print(f'The mult is: {mult}')
-print(f'The div is: {div}')
-print(f'The divInt is: {divInt}')
-print(f'The res is: {res}')
-print(f'The exp is: {exp}')
+miVAriable = miVAriable + 1
+print(miVAriable)
