5e2c4c2e8c6aeab872e07eeda7f3cf4a8c9dd62b
ezetina <jenzetin@gmail.com>
2022-08-24 07:50:42 -0500

    Iterating a list

--- Class5/main.py
+++ Class5/main.py
@@ -3,7 +3,10 @@ Working with lists in Python
 """
 # Defining a list of names
 
-names = ["John", "Henry", "Richard"]
+from cmath import pi
+
+
+names = ["John", "Henry", "Richard", "Lili"]
 
 # Printing the names list
 
@@ -14,3 +17,22 @@ print(names[0])
 
 # Printing a value of my list in reverse
 print(names[-1])
+
+# Print ranges from a list, it prints 0 an 1 values
+print(names[0:2])
+
+# Print from start without including the first element
+print(names[:3])
+
+# Print list until end
+print(names[1:])
+
+#Change valued from list
+names[1] = "Bobby"
+print(names)
+
+# Iterate a list
+for name in names:
+    print(name)
+else:
+    print("No more names!")
