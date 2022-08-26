8968d7d973cccf31d5633282d1a2c3172685d638
ezetina <jenzetin@gmail.com>
2022-08-26 07:33:14 -0500

    Working with dictionaries

--- Dictionaries/main.py
+++ Dictionaries/main.py
@@ -23,3 +23,38 @@ print(dictionary.get('OOP'))
 # Modifying elements
 dictionary["IDE"]= "integrated development environment"
 print(dictionary.get('IDE'))
+
+# Loop into a dictionary
+for key, value in dictionary.items():
+    print(key,value)
+    
+# Loop only keys
+for key in dictionary.keys():
+    print(key)
+
+# Loop only value
+for value in dictionary.values():
+    print(value)
+    
+# Verify if the element exists
+print("IDE" in dictionary)
+
+# Adding an element
+dictionary["PK"] = "Primary Key"
+print(dictionary)
+
+# Is not possible to add duplicate values
+dictionary["PK"] = "Primary Key"
+print(dictionary)
+
+# Delete an element
+dictionary.pop("PK")
+print(dictionary)
+
+# Delete all the elements
+dictionary.clear()
+print(dictionary)
+
+# Delete the dictionary
+del dictionary
+print(dictionary)
