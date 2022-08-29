f1408caaae19677674c0b168a03b330c21d8918f
ezetina <jenzetin@gmail.com>
2022-08-29 16:34:10 -0500

    Working with parameters and functions

---
+++ Functions/main.py
@@ -0,0 +1,16 @@
+# Define a function
+def my_function():
+    print("Hello World!")
+
+
+my_function()
+# It's possible to call a function multiple times
+my_function()
+
+
+def new_function(name, last_name):
+    print('Hello!', f'{name}', f'{last_name}')
+
+
+new_function('Henry', 'Zetina')
+new_function('Henry', 'Moya')
