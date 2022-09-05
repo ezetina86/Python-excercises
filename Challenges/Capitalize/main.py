718a12e853f5f081b7650af71e4cfc77a87d7fb1
ezetina <jenzetin@gmail.com>
2022-09-05 06:35:43 -0500

    title() function

--- Challenges/Capitalize/main.py
+++ Challenges/Capitalize/main.py
@@ -4,20 +4,9 @@ For example, alison heck should be capitalized correctly as Alison Heck."""
 
 
 def solve(s):
-    capitalized_string = ""
-    the_string = s.split()
-    print(the_string)
-    listOutput = []
-    for word in the_string:
-        word = word.capitalize()
-        print(word)
-        listOutput.append(word)
-        print(listOutput)
-    capitalized_string = ' '.join(listOutput)
-    print(capitalized_string)
-    return capitalized_string
+    return s.title()
 
 
 if __name__ == '__main__':
     s = input("Enter a String: \n")
-    solve(s)
+    print(solve(s))
