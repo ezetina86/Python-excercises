b329b87033d80b96ae3ee29d2ce6577274d5418b
ezetina <jenzetin@gmail.com>
2022-09-08 07:10:38 -0500

    working with classes

---
+++ POO/Person.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self):
+        self.name = 'Henry'
+        self.last_name = 'Zetina'
+        self.age = 36
+
+
+person1 = Person()
+print(person1.name)
+print(person1.last_name)
+print(person1.age)
