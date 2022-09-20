7ff609ad8ee22c5b81b1719626fe22af6b4c028d
ezetina <jenzetin@gmail.com>
2022-09-19 20:32:45 -0500

    Working with classes

--- POO/Person.py
+++ POO/Person.py
@@ -1,11 +1,18 @@
 class Person:
-    def __init__(self):
-        self.name = 'Henry'
-        self.last_name = 'Zetina'
-        self.age = 36
+    def __init__(self, name, last_name, age):
+        # Self is an attribute of the class
+        # The right side are parameters
+        self.name = name
+        self.last_name = last_name
+        self.age = age
 
 
-person1 = Person()
+person1 = Person("Henry", "Zetina", 36)
 print(person1.name)
 print(person1.last_name)
 print(person1.age)
+
+# Creating a second object
+
+person2 = Person('Lili', 'Castro', 37)
+print(f'Person 2: {person2.name} {person2.last_name} {person2.age}')
